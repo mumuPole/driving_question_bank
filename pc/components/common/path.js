fbfc0ab4c041c718850ad60d3bb7f5d4f5bb314c
panchong <panchong@migoedu.com>
2017-07-25 17:52:09 +0800

    初步模型

--- pc/components/common/path.js
+++ pc/components/common/path.js
@@ -2,6 +2,6 @@
  * Created by panchong on 17/7/20.
  */
 const PATH = {
-	testPage: '/testPage',
+    testPage: '/testPage',
 };
 module.exports = PATH;
--- pc/components/testPage/testPage.js
+++ pc/components/testPage/testPage.js
@@ -2,12 +2,13 @@
  * Created by panchong on 17/7/20.
  */
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, hashHistory } from 'react-router';
 import { Radio, Modal } from 'antd';
 import { loadQuestion } from '../common/loadQuestion';
 import './testPage.less';
 
 const RadioGroup = Radio.Group;
+const confirm = Modal.confirm;
 export default class TestPage extends React.Component {
     constructor() {
         super();
@@ -29,51 +30,77 @@ export default class TestPage extends React.Component {
     }
     onChangeChoose = e => {
     	this.setState({
-        choose: e.target.value,
-    });
+            choose: e.target.value,
+        });
     };
     correctAnswer = () => {
         const questions = this.state.questions;
         let index = this.state.index;
         let score = this.state.score;
-        const _this = this;
         if (this.state.choose === questions[index].answer) {
-            Modal.success({
-                title: '回答正确',
-                content: questions[index].explains,
-                onOk() {
-                    index += 1;
-                    _this.setState({
-                        index,
-                        choose: '',
-                    });
-                },
-            });
+			let time = 5;
+            const modal = Modal.success({
+				title: '回答正确',
+                content: <div>
+                    <p>{questions[index].explains}</p>
+                    <p>{`倒计时关闭窗口：${setInterval(function () {
+                        time -= 1;
+						console.log(time);
+                        return time;
+					}, 1000)}秒`}</p>
+                </div>,
+				onOk() {},
+			});
+			setTimeout(() => modal.destroy(), 5000);
             score += 1;
         } else {
-            Modal.error({
-                title: '回答错误',
-                content: questions[index].explains,
-                onOk() {
-                    index += 1;
-                    _this.setState({
-                        index,
-                        choose: '',
-                    });
-                },
-            });
+			let time = 5;
+            const modal = Modal.error({
+				title: '回答错误',
+				content: <div>
+                    <p>{questions[index].explains}</p>
+                    <p>{`倒计时关闭窗口：${setInterval(function () {
+						time -= 1;
+						console.log(time);
+						return time;
+					}, 1000)}秒`}</p>
+                </div>,
+				onOk() {},
+			});
+			setTimeout(() => modal.destroy(), 5000);
         }
+		index += 1;
         this.setState({
+			index,
+			choose: '',
             score,
         });
     };
-	handleRetest = () => {
-	    this.setState({
-			index: 0,
-			choose: '',
-			score: 0,
-        });
-    }
+    handleRetest = () => {
+        const _this = this;
+		confirm({
+			title: '你确定要重新测试吗?',
+			content: '本次测试成绩将归0',
+			onOk() {
+				_this.setState({
+					index: 0,
+					choose: '',
+					score: 0,
+				});
+			},
+			onCancel() {},
+		});
+    };
+    goBackIndex = () => {
+		confirm({
+			title: '你确定要返回首页吗?',
+			content: '本次测试成绩将归0',
+			onOk() {
+				hashHistory.push('/');
+			},
+			onCancel() {},
+		});
+    };
     render() {
     	const questions = this.state.questions;
         const index = this.state.index;
@@ -82,22 +109,26 @@ export default class TestPage extends React.Component {
             display: 'block',
         };
         return (<div className="testPage" >
+         <button className="yellowBtn" onClick={this.goBackIndex}>返回首页</button>
             {
-				questions.length === 0 ? null : <div>
-    <button className="blueBtn" onClick={this.handleRetest}>重新测试</button>
-                        <Link to="/"><button className="yellowBtn">返回首页</button></Link>
-    <p>{`第${index + 1}题`}</p>
-    <img alt="" src={question.url} />
-    <p>{question.question}</p>
+				questions.length === 0 ? null : <div className="questionItem">
+    <p className="questionNo">{`第${index + 1}题`}</p>
+                        <div  className="questionImg">
+                            <img alt="" src={question.url} />
+                        </div>
+    <p className="questionStem">{question.question}</p>
     <RadioGroup onChange={this.onChangeChoose} value={this.state.choose}>
         <Radio style={radioStyle} value="1">{`A.${question.item1}`}</Radio>
         <Radio style={radioStyle} value="2">{`B.${question.item2}`}</Radio>
         <Radio style={radioStyle} value="3">{`C.${question.item3}`}</Radio>
         <Radio style={radioStyle} value="4">{`D.${question.item4}`}</Radio>
     </RadioGroup>
-    <button className="blueBtn" onClick={this.correctAnswer}>下一题</button>
 					</div>
 			}
+            <div className="bottomBtn">
+                <button className="blueBtn" onClick={this.correctAnswer}>下一题</button>
+                <button className="grayBtn" onClick={this.handleRetest}>重新测试</button>
+            </div>
         </div>);
     }
 }
